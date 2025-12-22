d8ebc9efbfe0c627088c160702c91f4ccf0f82c2
Wang Fan <2953599032@qq.com>
2025-12-22 20:24:03 +0800

    Update validation data path and split in val.py

--- val.py
+++ val.py
@@ -14,8 +14,8 @@ def get_weight_size(path):
 if __name__ == '__main__':
     model_path = 'runs/train/exp/weights/best.pt'
     model = RTDETR(model_path) # 选择训练好的权重路径
-    result = model.val(data='/root/dataset/dataset_visdrone/data.yaml',
-                      split='test', # split可以选择train、val、test 根据自己的数据集情况来选择.
+    result = model.val(data='./datasets/A_drowning_person.yaml',
+                      split='val', # split可以选择train、val、test 根据自己的数据集情况来选择.
                       imgsz=640,
                       batch=4,
                     #   save_json=True, # if you need to cal coco metrice
