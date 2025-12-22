3b373965bd103845f9a960c21c88975c43fe4c6e
Wang Fan <2953599032@qq.com>
2025-12-22 20:20:32 +0800

    Remove model weights loading in train.py
    Removed loading of weights for the RTDETR model.

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from ultralytics import RTDETR
 
 
 if __name__ == "__main__":
-    model = RTDETR("my_cfg/rtdetr-r18.yaml").load("weights/rtdetr-r18.pt")
+    model = RTDETR("my_cfg/rtdetr-r18.yaml")
 
     model.train(
         data="dataset/A_drowning_person.yaml",
